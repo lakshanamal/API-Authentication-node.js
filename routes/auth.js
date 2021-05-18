765592feeb71d2c6b12c1a022d604d88a69c7ade
lakshanamal <lakshanamal100@gmail.com>
2021-05-18 19:44:04 +0530

    encrypt my password

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,37 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const bcrypt=require('bcrypt');
+const { authValidation } = require("../validation");
 
 //validation
 
-const Joi = require("@hapi/joi");
-const { Schema } = require("mongoose");
-
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
-
 router.post("/register", async (req, res) => {
   // lets validate the data before we a user
-  const { error } = schema.validate(req.body); // error eka withari enne meken
-  if (error) {return res.status(400).send(error.details[0].message);}
-  else{
+  const { error } = authValidation(req.body); // error eka withari enne meken
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  // check user exesit in database
+  const checkUser=await  User.findOne({email:req.body.email});
+  if(checkUser) return res.status(400).send("User alredy in the system");
+
+  // password encryption
+  const salt= await bcrypt.genSalt(10);
+  const hashedPassword=await bcrypt.hash(req.body.password,salt);
+  console.log(hashedPassword);
+
     const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
       });
     
       try {
-        const savedUser = await newUser.save();
+        const savedUser = await newUser.save(); // mongoose required
         res.send(savedUser);
       } catch (err) {
         res.status(400).send(err);
       }
-  }
-
- 
-});
+  });
 
 // router.get('/',(req,res)=>{
 //     const newUser = new User({
---
+++ validation.js
@@ -0,0 +1,15 @@
+const Joi = require("@hapi/joi");
+
+
+// regiter feild validation
+const authValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports.authValidation = authValidation;
