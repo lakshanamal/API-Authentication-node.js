71fee2d3d974416bfbd2ab2e1d72cb80eb5b6e63
lakshanamal <lakshanamal100@gmail.com>
2021-05-18 17:41:49 +0530

    database connection added

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
 
+dotenv.config();
+
+// middle ware  
 app.use(express.json());
-// Import router 
-const authRoute=require('./routes/auth');
+// database connrction
+mongoose.connect(process.env.DB_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// Import router
+const authRoute = require("./routes/auth");
 
 // rorute middleWare
-app.use('/api/user',authRoute);
+app.use("/api/user", authRoute);
 
-
-
-app.listen(3001,()=>{
-    console.log("Server runnung port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server runnung port 3000");
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,32 @@
 const router =require('express').Router();
+const User=require('../model/User');
 
-router.post('/register',(req,res)=>{
-    res.send("Register sucess");
+router.post('/register', async (req,res)=>{
+    const newUser = new User({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    });
+
+    try{
+        const savedUser=await User.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
+
+    newUser.save();
+   
 })
+
+// router.get('/',(req,res)=>{
+//     const newUser = new User({
+//         name:req.body.name,
+//         email:req.body.email,
+//         passward:req.body.passward
+//     })
+   
+// })
 
 
 module.exports=router;
